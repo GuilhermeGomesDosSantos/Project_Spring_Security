e15bf97b4175c762417fae3748b2c86f874efa02
GuilhermeGomesDosSantos <gomes3654gui@gmail.com>
2026-02-08 19:43:12 -0300

    Iniciando Projeto Spring Security

---
+++ src/main/java/com/secureauth/secure_access_api/SecureAccessApiApplication.java
@@ -0,0 +1,13 @@
+package com.secureauth.secure_access_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureAccessApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureAccessApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/secureauth/secure_access_api/SecureAccessApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.secureauth.secure_access_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureAccessApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
