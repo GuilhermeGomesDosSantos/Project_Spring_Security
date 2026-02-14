a70bd558e7bd623b59aba0d9da6e249705718ddf
GuilhermeGomesDosSantos <gomes3654gui@gmail.com>
2026-02-14 17:16:42 -0300

    Gerando Tokens JWT

--- src/main/java/com/secureauth/secure_access_api/main/controller/AuthenticationController.java
+++ src/main/java/com/secureauth/secure_access_api/main/controller/AuthenticationController.java
@@ -1,9 +1,10 @@
 package com.secureauth.secure_access_api.main.controller;
 
 import com.secureauth.secure_access_api.main.domain.user.DTOauthentication;
+import com.secureauth.secure_access_api.main.domain.user.User;
+import com.secureauth.secure_access_api.main.infra.exception.security.tokenService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -14,11 +15,15 @@ import org.springframework.web.bind.annotation.*;
 public class AuthenticationController {
     @Autowired
     AuthenticationManager manager;
+
+    @Autowired
+    tokenService tokenService;
+
     @PostMapping
     public ResponseEntity authenticationUser(@RequestBody @Valid DTOauthentication dtOauthentication) {
         var token = new UsernamePasswordAuthenticationToken(dtOauthentication.login(), dtOauthentication.password());
         var authentication = manager.authenticate(token);
 
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok(tokenService.generateToken((User) authentication.getPrincipal()));
     }
 }
---
+++ src/main/java/com/secureauth/secure_access_api/main/infra/exception/security/tokenService.java
@@ -0,0 +1,31 @@
+package com.secureauth.secure_access_api.main.infra.exception.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.secureauth.secure_access_api.main.domain.user.User;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class tokenService {
+    public String generateToken(User user){
+        try {
+            var algoritmo = Algorithm.HMAC256("12345");
+             return JWT.create()
+                    .withIssuer("API Security GDS")
+                     .withSubject(user.getLogin())
+                     .withExpiresAt(dataExpiration())
+                    .sign(algoritmo);
+        } catch (JWTCreationException exception){
+            throw new RuntimeException("Error to generat JWT Token");
+        }
+    }
+
+    private Instant dataExpiration() {
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+}
